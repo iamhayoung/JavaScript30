63f2b5413b208b4a7fb7ccf4e323e11cec057aea
Hayoung Gam <hahhyoung@gmail.com>
2021-03-13 15:20:34 +0900

    HTML 지정

--- 03 - CSS Variables/app.js
+++ 03 - CSS Variables/app.js
@@ -1,28 +1,15 @@
 const spacing = document.querySelector('#spacing');
 const blur = document.querySelector('#blur');
 const base = document.querySelector('#base');
-const root = document.querySelector('html');
-
-// spacing.onchange = handleSpacing;
-// blur.onchange = handleBlur;
-// base.onchange = handleBase;
-
-// function handleSpacing(e) {
-//   root.style.setProperty('--spacing', `${ e.target.value }px`);
-// }
-
-// function handleBlur(e) {
-//   root.style.setProperty('--blur', `${ e.target.value }px`);
-// }
 
 spacing.addEventListener('input', (e) => {
-  root.style.setProperty('--spacing', `${ e.target.value }px`);
+  document.documentElement.style.setProperty('--spacing', `${ e.target.value }px`);
 })
 
 blur.addEventListener('input', (e) => {
-  root.style.setProperty('--blur', `${ e.target.value }px`);
+  document.documentElement.style.setProperty('--blur', `${ e.target.value }px`);
 })
 
 base.addEventListener('input', (e) => {
-  root.style.setProperty('--base', `${e.target.value}`);
+  document.documentElement.style.setProperty('--base', `${e.target.value}`);
 })
